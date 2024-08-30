23537eaf7257bf68f9302c129dfc46768415743a
tranquyenbk173 <tranquyenhd17@gmail.com>
2024-08-30 17:29:39 +0000

    fix_sbatch

--- run1.sh
+++ run1.sh
@@ -1,20 +1,24 @@
 #!/bin/bash -e
 
-# SBATCH --job-name=abcxyz # create a short name for your job
-# SBATCH --output=/home/quyentt15/quyentt15/Bayesian_finetuning/mbpp%A.out # create a output file
-# SBATCH --error=/home/quyentt15/quyentt15/Bayesian_finetuning/mbpp%A.err # create a error file
-# SBATCH --partition=research # choose partition
-# SBATCH --gpus-per-node=1
-# SBATCH --cpus-per-task=20
-# SBATCH --mem-per-gpu=40GB
-# SBATCH --nodes=1
-# SBATCH --ntasks=1
-# SBATCH --time=10-00:00          # total run time limit (DD-HH:MM)
-# SBATCH --mail-type=begin        # send email when job begins
-# SBATCH --mail-type=end          # send email when job ends
-# SBATCH --mail-type=fail          # send email when job fails
-# SBATCH --mail-user=v.quyentt15@vinai.io
+#SBATCH --job-name=abcxyz # create a short name for your job
+#SBATCH --output=/home/quyentt15/quyentt15/Bayesian_finetuning/mbpp%A.out # create a output file
+#SBATCH --error=/home/quyentt15/quyentt15/Bayesian_finetuning/mbpp%A.err # create a error file
+#SBATCH --partition=research # choose partition
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-gpu=40GB
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=10-00:00          # total run time limit (DD-HH:MM)
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-type=fail          # send email when job fails
+#SBATCH --mail-user=v.quyentt15@vinai.io
+
+module load python/miniconda3/miniconda3   
+eval "$(conda shell.bash hook)" 
 conda activate coda
+cd /home/quyentt15/quyentt15/Bayesian_finetuning/
 
 python main.py fit --config configs/lora/cifar100-r16-lr-0.05_svgd0.yaml
 
